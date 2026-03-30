234d26d41c3b6272eee6ad9778341549d95c796c
Mohanavelan S <85865915+mohanavelan-s@users.noreply.github.com>
2026-03-30 17:43:18 +0530

    Fix Android Gradle wrapper and theme setup

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -146,6 +146,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
+    implementation("com.google.android.material:material:1.12.0")
 
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
